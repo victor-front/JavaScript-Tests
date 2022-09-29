7f449d9c300e1943d445411f9c49d3f7f5819477
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-09-28 21:33:30 -0300

    Base

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function main(){
-	console.log("Main carregado com exito.");
+	let projetos = {
+		nomes: ["Luara,", "Eu", "TE", "Amo<3"],
+		caminhos: ["#", "#", "#", "#"],
+	}
+	
+	for(c=0;c<projetos.nomes.length;c++){
+		let prodiv = document.getElementById('projetos');
+		let caminho = document.createElement('a');
+		caminho.setAttribute('id', 'caminho');
+		caminho.setAttribute('href', `${projetos.caminhos[c]}`);
+		caminho.setAttribute('target', '_blank');
+		caminho.innerHTML = `${projetos.nomes[c]}`;
+		caminho.classList.add('proj-caminho');
+		prodiv.appendChild(caminho);
+	}
 }
