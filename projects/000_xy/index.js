e9bcabe4c2348e00dc010e26b8ba9c62e5921d1c
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-10-09 18:52:32 -0300

    eix

--- projects/000_xy/index.js
+++ projects/000_xy/index.js
@@ -1,3 +1,17 @@
-function get(){
-	console.log('nothing');
-};
+let el = document.getElementById('player');
+console.log(el);
+let cords = el.getBoundingClientRect();
+console.log(cords);
+let plu = 0;
+
+function mov(){
+	el.style.top = `${plu}px`;
+	el.style.left = `${plu}px`;
+	el.style.transform = `rotate(${plu}deg)`;
+	plu+=1;
+	setTimeout(()=>{
+		mov();
+	}, .5)
+}
+
+mov();
