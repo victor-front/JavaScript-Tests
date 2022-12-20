cca0f67e92c64be3f92dc872bb6bc70d89fd2db7
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-12-20 19:21:10 -0300

    mas uno

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function main(){
 	let projetos = {
-		nomes: ["Eixos", "Anchor Park Demo"],
-		caminhos: ["projects/000_xy/index.html", "projects/001_anchor_park_piloto/index.html"]
+		nomes: ["Eixos", "Anchor Park Demo", "Teste del día"],
+		caminhos: [
+		"projects/000_xy/index.html", 
+		"projects/001_anchor_park_piloto/index.html",
+		"projects/003_pratica_del_dia/index.html"]
 	}
 	
 	for(c=0;c<projetos.nomes.length;c++){
---
+++ projects/003_pratica_del_dia/script.js
@@ -0,0 +1,6 @@
+function rodar(){
+	document.getElementById('edimsg').innerHTML = 'doki dokikkkk';
+	for(c=0;c<40;c++){
+		document.getElementById('add').innerHTML += `<p>Colorado</p>`;
+	}
+}
