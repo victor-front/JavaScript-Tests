db0dfe2be9f5987552ee78ed679a0d9f1e585e13
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-12-23 19:55:38 -0300

    outr

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function main(){
 	let projetos = {
-		nomes: ["Eixos", "Anchor Park Demo", "Teste del día"],
+		nomes: ["Eixos", "Anchor Park Demo", "Teste del día", "Outro testekk"],
 		caminhos: [
 		"projects/000_xy/index.html", 
 		"projects/001_anchor_park_piloto/index.html",
-		"projects/003_pratica_del_dia/index.html"]
+		"projects/003_pratica_del_dia/index.html",
+		"projects/004_prac/index.html"
+		]
 	}
 	
 	for(c=0;c<projetos.nomes.length;c++){
