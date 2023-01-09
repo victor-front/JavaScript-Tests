9ae4ee7f94d5897303db7563c9bef999266ba7f8
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-01-09 20:06:48 -0300

    se

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function main(){
 	let projetos = {
-		nomes: ["Eixos", "Anchor Park Demo", "Teste del día", "Outro testekk"],
+		nomes: ["Eixos", "Anchor Park Demo", "Teste del día", "Outro testekk", "Choicengine"],
 		caminhos: [
 		"projects/000_xy/index.html", 
 		"projects/001_anchor_park_piloto/index.html",
 		"projects/003_pratica_del_dia/index.html",
-		"projects/004_prac/index.html"
+		"projects/004_prac/index.html",
+		"projects/005_choicengine/index.html"
 		]
 	}
 	
